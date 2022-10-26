30fdeef5672f0374fc49acb0999efdc58325c57a
Ahram-Kim <ahram0223@gmail.com>
2022-10-27 01:35:30 +0900

    2022.10.27 ~43:47

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,3 +1,9 @@
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import PlayPause from './PlayPause';
+import { playPuase, setActiveSongs } from '../redux/features/playerSlice';
+
 const SongCard = () => (
   <div>SongCard</div>
 );
